update: minor changes to fit qwen2; fix causal attn config bug

scripts/focal/init_dam_backbone.py
@@ -12,7 +12,11 @@ import copy
 
 size = "7"
 save_dir = f"./checkpoints/Qwen2-VL-{size}B-Dam"
-basemodel = f"Qwen/Qwen2-VL-{size}B-Instruct"
+if size == "7":
+    basemodel = f"Qwen/Qwen2-VL-7B-Instruct"
+elif size == "2"
+    basemodel = f"/mnt/tidal-alsh01/dataset/mmeb/Qwen2-VL-2B-Instruct"
+
 
 
 base_model = Qwen2VLRetForConditionalGeneration.from_pretrained(basemodel,low_cpu_mem_usage=False,  attn_implementation="flash_attention_2", torch_dtype=torch.bfloat16)

scripts/focal/merge_dam_lamra.py
@@ -14,15 +14,16 @@ import os
 current_file_path = os.path.dirname(os.path.abspath(__file__))
 module_path = os.path.join(current_file_path, "../../")
 sys.path.append(module_path)
-from models.qwen2_vl import Qwen2VLForConditionalGeneration
+from models.qwen2_vl import Qwen2VLRetForConditionalGeneration
 
 import torch
 import copy
 
 # generate pretrain weights for mbeir finetuning
-save_dir = "./tmp_ckpts/dam_cvp_global_nilpretrain"
-lamra_model = "./checkpoints/LEMUIR_Pretrain"
-vision_backbone = "./checkpoints/qwen2_5-vl-7b_DAM_vit_c+v+p_1e-4"
+save_dir = "./tmp_ckpts/qwen2-2b_DAM_cpenc_subnil"
+lamra_model = "../dam-qwen2/checkpoints/qwen2-vl-2b_temp0.07_stagel1-5"
+# vision_backbone = "checkpoints/qwen2-vl-2b_DAM_cp"
+vision_backbone = "checkpoints/qwen2-vl-2b_DAM_cp_enc/checkpoint-500"
 
 # generate ctx only
 # save_dir = "./tmp_ckpts/dam_pretrain+lamrallm"
@@ -39,11 +40,11 @@ print("@gt attn_factor: ", base_model.visual.context_layers[0].attn_factor)
 
 lamra_ret.save_pretrained(save_dir)
 
-base_model = Qwen2_5_VLRetForConditionalGeneration.from_pretrained(save_dir,low_cpu_mem_usage=False,  attn_implementation="flash_attention_2", torch_dtype=torch.bfloat16)
+base_model = Qwen2VLRetForConditionalGeneration.from_pretrained(save_dir,low_cpu_mem_usage=False,  attn_implementation="flash_attention_2", torch_dtype=torch.bfloat16)
 
-processor = AutoProcessor.from_pretrained("./checkpoints/LEMUIR_Pretrain")
+processor = AutoProcessor.from_pretrained(lamra_model)
 processor.save_pretrained(save_dir)  # 这一句会把preprocessor_config.json、chat_template.json等复制过来
-tokenizer = AutoTokenizer.from_pretrained("./checkpoints/LEMUIR_Pretrain")
+tokenizer = AutoTokenizer.from_pretrained(lamra_model)
 tokenizer.save_pretrained(save_dir)
 
 # python scripts/focal/merge_dam_lamra.py